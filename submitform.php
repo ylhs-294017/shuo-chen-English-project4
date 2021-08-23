8e08b52b1413e8fe4e6ecf1e63ea342ab5aa281a
vivjamba <85517762+vivjamba@users.noreply.github.com>
2021-08-23 09:58:13 -0400

    email function v2

--- submitform.php
+++ submitform.php
@@ -1,12 +1,9 @@
 <?php 
 ob_start(); 
 error_log();
-if(isset($_POST['name']) && isset($_POST['email'])){
+if(isset($_POST['email'])){
 
-$name = $_POST['name'];
 $email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
 
 $mailHeading = 'MyFreeOnlineTools Contact Form';
 $headers = 'MIME-Version: 1.0' . "\r\n";
@@ -14,13 +11,10 @@ $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 $headers .= 'From: MyFreeOnlineTools <mailmyfreeonlinetools.com>' . PHP_EOL .
 'Reply-To: MyFreeOnlineTools <mailmyfreeonlinetools.com>' . PHP_EOL .
 'X-Mailer: PHP/' . phpversion();
-
 $to = 'cutev91011@gmail.com'; 
+$messageSent = '<div> Name Email : '.$email.' </div>';
 
-
-$messageSent = '<div>Name Sender : '.$name .'</div><div> Name Email : '.$email.' </div><div>Mail Subject : '.$subject.'</div><div> Message : <br/> '.$message.'</div>';
-
-if( $name != '' || $message != '' || $email != ''){
+if($email != ''){
 mail($to,$subject,$messageSent,$headers);
 echo 'Mail Send I Will Get Back to You....';
 } 
